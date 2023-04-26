797aa5168fe78601b69cdf16af0ac75500e4ae5f
SARA453 <sara.davoody@gmail.com>
2023-04-26 22:13:44 +0200

    design

--- src/components/Class.jsx
+++ src/components/Class.jsx
@@ -1,7 +1,7 @@
 const Class = ({ className, asset, onClick }) => {
   return (
     <div onClick={onClick} className="flex flex-col items-center  p-2 m-2 ">
-      <img src={asset.url} className=" rounded h-52" />
+      <img src={asset.url} className=" rounded h-52 mb-7" />
       <p className="text-green-400 font-['Roboto-Regular']">
         {className}
       </p>
--- src/pages/ClassDetails.jsx
+++ src/pages/ClassDetails.jsx
@@ -1,4 +1,3 @@
-
 import { useParams, navigate, useNavigate } from "react-router";
 import useMutation from "../hooks/useMutation";
 import useQuery from "../hooks/useQuery";
@@ -13,7 +12,7 @@ const ClassDetails = () => {
 
   const { mutate } = useMutation(addUserToClass, () => {
     alert("signup ok");
-    navigate("/myschedule")
+    navigate("/myschedule");
   });
 
   const handleAddToClass = async () => {
@@ -27,10 +26,10 @@ const ClassDetails = () => {
   const userIsMember = details?.users.find((item) => item.id == userId);
 
   return (
-    <div className="container">
-      <div className="flex flex-col items-center justify-center">
+    <div className="container mx-auto">
+      <div className=" bg-red-200 flex flex-col items-center justify-center mt-14">
         <div className="relative">
-          <img src={details?.asset && details?.asset.url} />
+          <img src={details?.asset && details?.asset.url} className="mt-14" />
           {!userIsMember && (
             <button
               onClick={handleAddToClass}
@@ -42,22 +41,32 @@ const ClassDetails = () => {
         </div>
 
         <div>
-          <p className="text-3xl mt-3 text-green-400 font-['Roboto-Regular']">Schedule</p>
+          <p className="text-3xl mt-3 text-green-400 font-['Roboto-Regular']">
+            Schedule
+          </p>
           <div className="flex gap-[90px] ">
-            <p className="text-green-400 font-['Roboto-Regular']">{details?.classDay}</p>
-            <p className="text-green-400 font-['Roboto-Regular']">{details?.classTime}</p>
+            <p className="text-green-400 font-['Roboto-Regular']">
+              {details?.classDay}
+            </p>
+            <p className="text-green-400 font-['Roboto-Regular']">
+              {details?.classTime}
+            </p>
           </div>
         </div>
         <div>
-          <p className="mt-5 text-green-400 font-['Roboto-Regular'] ">{details?.classDescription}</p>
+          <p className="mt-5 text-green-400 font-['Roboto-Regular'] ">
+            {details?.classDescription}
+          </p>
         </div>
 
         <div className="flex justify-between">
           <div className="flex flex-col">
-            <p className="text-3xl mt-12 text-green-400 font-['Roboto-Regular']">Trainer</p>
+            <p className="text-3xl mt-12 text-green-400 font-['Roboto-Regular']">
+              Trainer
+            </p>
 
             <img src={details?.trainer.asset.url} className="h-36 w-28 mt-5 " />
-            <p className="mt-5 text-green-400 font-['Roboto-Regular']">
+            <p className="my-7 text-green-400 font-['Roboto-Regular']">
               {details?.trainer.trainerName}
             </p>
           </div>
